c859f6b2460f906fe86f01ed1778da5e9ba10738
최종현 <rhrhdksk@gmail.com>
2024-08-27 09:29:10 +0900

    fix sitemap.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
 			userAgent: "*",
 			allow: "/",
 		},
-		sitemap: "https://hyune.vercel.app/sitemap.xml",
+		sitemap: "https://bittenlog.vercel.app/sitemap.xml",
 	}
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,17 @@
 import type { MetadataRoute } from "next"
 import { useReadMdx as ReadMDX } from "../hooks/useReadMdx"
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-	const baseUrl = "https://hyune.vercel.app"
+	const baseUrl = "https://bittenlog.vercel.app"
 
 	const posts = await ReadMDX()
 	const indexPage = {
 		url: baseUrl,
-		lastModified: new Date(),
+		lastModified: new Date().toISOString().split("T")[0],
 	}
 	const article = posts.map((post) => {
 		return {
 			url: `${baseUrl}/article/${post.path}`,
-			lastModified: post.date,
+			lastModified: new Date(post.date).toISOString().split("T")[0],
 		}
 	})
 
